7511b6eff3614acabada48cfa4248b8dde188a26
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-12-12 23:28:47 +0530

    First Section creating

--- src/Home Page/Entrypage.jsx
+++ src/Home Page/Entrypage.jsx
@@ -38,6 +38,30 @@ export default function Entrypage() {
                             </div>
                         </Link>
                     </div>
+
+                </div>
+                <div className="jefbhhbsvcs">
+                    BISWAYAN MAZUMDER
+                </div>
+                <div className="kdnfndv">
+                    <svg width="14px" height="14px" viewBox="0 0 14 14" version="1.1">
+                        <title>arrow-up-right</title>
+                        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                            <g transform="translate(-1019.000000, -279.000000)" stroke="#FFFFFF" strokeWidth="1.5">
+                                <g transform="translate(1026.000000, 286.000000) rotate(90.000000) translate(-1026.000000, -286.000000) translate(1020.000000, 280.000000)">
+                                    <polyline points="2.76923077 0 12 0 12 9.23076923"></polyline>
+                                    <line x1="12" y1="0" x2="0" y2="12"></line>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+
+                </div>
+                <div className='dnjndjns'>
+                    Mobile App Developer
+                </div>
+                <div className='dnjndjnq'>
+                    Web Developer
                 </div>
                 <img src="https://g1uudlawy6t63z36.public.blob.vercel-storage.com/e64edd025438449584ac6c481eafa22d.png" alt="hbdhbhb" height={"100%"} />
             </div>
